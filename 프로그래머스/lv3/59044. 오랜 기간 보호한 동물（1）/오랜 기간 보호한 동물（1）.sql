d05a8f03c6cb486dcc1bde5cbb34bac61a0fcf6d
AlmondGood <88735581+almondgood@users.noreply.github.com>
2022-08-15 13:24:26 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT ins.name, ins.datetime
+FROM animal_ins ins
+LEFT OUTER JOIN animal_outs outs ON ins.animal_id = outs.animal_id
+WHERE outs.animal_id is null
+ORDER BY ins.datetime LIMIT 3
