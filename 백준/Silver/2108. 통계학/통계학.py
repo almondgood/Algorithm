9f0873763111413875537c8e157e50acd99f48e0
AlmondGood <88735581+almondgood@users.noreply.github.com>
2024-03-01 19:21:22 +0900

    [Silver III] Title: 통계학, Time: 512 ms, Memory: 52796 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -2,36 +2,34 @@ from sys import stdin
 
 n = int(stdin.readline())
 
-
 numbers = []
 num_cnt = {}
 for i in range(n):
     num = int(stdin.readline())
     numbers.append(num)
+    
     if num not in num_cnt:
         num_cnt[num] = 0
     num_cnt[num] += 1
         
 numbers.sort()
-
-# 최빈값    
-# (num, cnt)
-fir, sec = ((0, 0), (0, 0))
+       
+max_cnt = 0 
+mode = []
 for num, cnt in num_cnt.items():
-    if cnt > sec[1]:
-        sec = (num, cnt)
-    elif cnt == sec[1] and num < sec[0]:
-        sec = (num, cnt)
-    
-    if fir[1] < sec[1]:
-        fir, sec = sec, fir
-    elif fir[1] == sec[1] and fir[0] > sec[0]:
-        fir, sec = sec, fir
-
+    if cnt > max_cnt:
+        max_cnt = cnt
+        mode = []
+        mode.append(num)
         
+    elif cnt == max_cnt:
+        mode.append(num)
+        
+mode.sort()
+
 avg = round(sum(numbers) / n)
 mid = numbers[n // 2]
-mode = fir[0] if n == 1 or fir[1] != sec[1] else sec[0]
+mode = mode[0] if len(mode) == 1 else mode[1]
 ran = numbers[-1] - numbers[0]   
         
 
