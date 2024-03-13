e3813b995adaa0e066fdc268f406e871502214e2
AlmondGood <88735581+almondgood@users.noreply.github.com>
2024-03-13 13:13:52 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31328 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,16 @@
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10**6)
+
+n = int(stdin.readline())
+
+memo = [0, 1, 2]
+def fibo(n):
+    if len(memo) > n:
+        return memo[n]
+    
+    memo.append(fibo(n-1) + fibo(n-2))
+    
+    
+    return memo[-1]
+
+print(fibo(n) % 10007)
