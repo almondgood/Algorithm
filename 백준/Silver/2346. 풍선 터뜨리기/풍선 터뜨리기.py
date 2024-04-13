8d39cf21d49e4ca420e3272df4bd391c0ddca5b4
AlmondGood <88735581+almondgood@users.noreply.github.com>
2024-04-13 13:52:38 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 64 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -4,20 +4,13 @@ from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
+d = deque(enumerate(map(int, input().split())))
 
-d = deque([i for i in range(0, n)])
-coms = list(map(int, input().split()))
-
-for i in range(0, n):
-  num = d.popleft()
-  print(num + 1)
+while d:
+  i, move = d.popleft()
+  print(i + 1)
   
-  if i == n - 1:
-    break
-  
-  if coms[num] > 0:
-    for j in range(0, coms[num] - 1):
-      d.append(d.popleft())
+  if move > 0:
+    d.rotate(-(move - 1))
   else:
-    for j in range(0, -coms[num]):
-      d.appendleft(d.pop())
+    d.rotate(-move)
