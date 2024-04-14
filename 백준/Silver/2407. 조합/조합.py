95dc33af65b4c9cefba42665ee7d0a24e48bb9ed
AlmondGood <88735581+almondgood@users.noreply.github.com>
2024-04-14 19:32:47 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,8 +1,10 @@
 import sys
-import math
 
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
 
-print(math.comb(n, m))
+li = [1]
+for i in range(1, n + 1):
+  li.append(li[-1] * i)
+
+print(li[n] // (li[m] * li[n-m]))
